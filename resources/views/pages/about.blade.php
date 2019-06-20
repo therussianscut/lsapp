753e5b2fadf796c4af825dda636810be94d49876
Sean McCann <smccann@athium.com>
2019-06-20 13:16:49 +0100

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('title', 'About')
+
+
+
+@section('content')
+
+    <p> This is the about page</p>
+
+
+@endsection
+
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+
+@section('title', 'Index')
+
+
+
+@section('content')
+
+    <p> This is the Index page</p>
+
+
+@endsection
